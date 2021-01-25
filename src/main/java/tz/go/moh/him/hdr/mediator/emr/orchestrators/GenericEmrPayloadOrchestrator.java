0ef40a508ce391e6a2ef6aaccc350f6d2a9dc198
cozej4 <ilakozejumanne@gmail.com>
2021-01-25 15:51:00 +0300

    Fixed codacy issues

--- src/main/java/tz/go/moh/him/hdr/mediator/emr/orchestrators/GenericEmrPayloadOrchestrator.java
+++ src/main/java/tz/go/moh/him/hdr/mediator/emr/orchestrators/GenericEmrPayloadOrchestrator.java
@@ -7,7 +7,11 @@ import akka.event.Logging;
 import akka.event.LoggingAdapter;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
 import org.openhim.mediator.engine.MediatorConfig;
+import org.openhim.mediator.engine.messages.FinishRequest;
 import org.openhim.mediator.engine.messages.MediatorHTTPRequest;
 import org.openhim.mediator.engine.messages.MediatorHTTPResponse;
 import tz.go.moh.him.hdr.mediator.emr.domain.EmrPayload;
@@ -17,6 +21,7 @@ import tz.go.moh.him.mediator.core.domain.ResultDetail;
 import tz.go.moh.him.mediator.core.utils.StringUtils;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -48,6 +53,11 @@ public class GenericEmrPayloadOrchestrator extends UntypedActor {
     protected List<ErrorMessage> errorMessages = new ArrayList<>();
 
     /**
+     * Represents an Error Messages Definition Resource Object defined in <a href="file:../resources/error-messages.json">/resources/error-messages.json</a>.
+     */
+    protected JSONObject errorMessageResource;
+
+    /**
      * The request handler that handles requests and responses.
      */
     private ActorRef requestHandler;
@@ -59,6 +69,14 @@ public class GenericEmrPayloadOrchestrator extends UntypedActor {
      */
     public GenericEmrPayloadOrchestrator(MediatorConfig config) {
         this.config = config;
+        InputStream stream = getClass().getClassLoader().getResourceAsStream("error-messages.json");
+        try {
+            if (stream != null) {
+                errorMessageResource = new JSONObject(IOUtils.toString(stream));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -87,32 +105,58 @@ public class GenericEmrPayloadOrchestrator extends UntypedActor {
                 errorMessages.add(errorMessage);
             }
 
-            EmrPayload emrPayload = objects.get(0);
-            //Reading the messageType so as to route the payload to the correct orchestrator
+            //Checking if there were any objects successfully converted POJOs
+            if (objects.size() > 0) {
 
-            ActorRef actor = null;
-            switch (emrPayload.getMessageType()) {
-                case "BEDOCC":
-                    actor = getContext().actorOf(Props.create(BedOccupancyOrchestrator.class, config));
-                    log.info("Forwarding request to: " + BedOccupancyOrchestrator.class.getSimpleName());
-                    break;
-                case "DDC":
-                    actor = getContext().actorOf(Props.create(DailyDeathCountOrchestrator.class, config));
-                    log.info("Forwarding request to: " + DailyDeathCountOrchestrator.class.getSimpleName());
-                    break;
-                case "REV":
-                    actor = getContext().actorOf(Props.create(RevenueReceivedOrchestrator.class, config));
-                    log.info("Forwarding request to: " + RevenueReceivedOrchestrator.class.getSimpleName());
-                    break;
-                case "SVCREC":
-                    actor = getContext().actorOf(Props.create(ServiceReceivedOrchestrator.class, config));
-                    log.info("Forwarding request to: " + ServiceReceivedOrchestrator.class.getSimpleName());
-                    break;
+                //Obtaining a single object as a sample of objects in the payloads
+                EmrPayload emrPayload = objects.get(0);
+
+                //Reading the messageType so as to route the payload to the correct orchestrator
+                ActorRef actor;
+                switch (emrPayload.getMessageType()) {
+                    case "BEDOCC":
+                        actor = getContext().actorOf(Props.create(BedOccupancyOrchestrator.class, config));
+                        log.info("Forwarding request to: " + BedOccupancyOrchestrator.class.getSimpleName());
+                        break;
+                    case "DDC":
+                        actor = getContext().actorOf(Props.create(DailyDeathCountOrchestrator.class, config));
+                        log.info("Forwarding request to: " + DailyDeathCountOrchestrator.class.getSimpleName());
+                        break;
+                    case "REV":
+                        actor = getContext().actorOf(Props.create(RevenueReceivedOrchestrator.class, config));
+                        log.info("Forwarding request to: " + RevenueReceivedOrchestrator.class.getSimpleName());
+                        break;
+                    case "SVCREC":
+                        actor = getContext().actorOf(Props.create(ServiceReceivedOrchestrator.class, config));
+                        log.info("Forwarding request to: " + ServiceReceivedOrchestrator.class.getSimpleName());
+                        break;
+
+                    default:
+                        actor = null;
+                }
+
+                //Forwarding the request to the correct orchestrator based on the payload message type
+                if (actor != null) {
+                    actor.tell(msg, getSelf());
+                } else {
+                    ErrorMessage errorMessage = new ErrorMessage(
+                            originalRequest.getBody(),
+                            Arrays.asList(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, errorMessageResource.getString("ERROR_MESSAGE_TYPE_IS_UNDEFINED"), null))
+                    );
+                    errorMessages.add(errorMessage);
+                }
+            } else { //In-case the conversion to POJO was not successful
+                ErrorMessage errorMessage = new ErrorMessage(
+                        originalRequest.getBody(),
+                        Arrays.asList(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, errorMessageResource.getString("ERROR_INVALID_PAYLOAD"), null))
+                );
+                errorMessages.add(errorMessage);
             }
 
-            //Forwarding the request to the correct orchestrator based on the payload message type
-            if (actor != null) {
-                actor.tell(msg, getSelf());
+            //If any error messages were encountered, returning the error messages to the Core
+            if (!errorMessages.isEmpty()) {
+                FinishRequest finishRequest = new FinishRequest(new Gson().toJson(errorMessages), "text/plain", HttpStatus.SC_BAD_REQUEST);
+                (originalRequest).getRequestHandler().tell(finishRequest, getSelf());
             }
 
         } else if (msg instanceof MediatorHTTPResponse) { //respond
