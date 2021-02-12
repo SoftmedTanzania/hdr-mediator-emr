f288bb524c79d9b04715cc33f4d84ddb0af760cb
cozej4 <ilakozejumanne@gmail.com>
2021-02-12 11:07:07 +0300

    Refactored domains

--- src/main/java/tz/go/moh/him/hdr/mediator/emr/domain/EmrPayload.java
+++ src/main/java/tz/go/moh/him/hdr/mediator/emr/domain/EmrPayload.java
@@ -1,5 +1,6 @@
 package tz.go.moh.him.hdr.mediator.emr.domain;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -9,6 +10,7 @@ public class EmrPayload {
      * Defines the Message type
      */
     @JsonProperty("Message Type")
+    @JsonAlias("MESSAGE TYPE")
     private String messageType;
 
     public String getMessageType() {
