b3bca668f315380dca20c8e4e544715535857e2f
cozej4 <ilakozejumanne@gmail.com>
2021-05-20 14:06:49 +0300

    Minor bugs fixes

--- src/main/java/tz/go/moh/him/hdr/mediator/emr/orchestrators/DeathByDiseaseCasesWithinFacilityOrchestrator.java
+++ src/main/java/tz/go/moh/him/hdr/mediator/emr/orchestrators/DeathByDiseaseCasesWithinFacilityOrchestrator.java
@@ -51,8 +51,8 @@ public class DeathByDiseaseCasesWithinFacilityOrchestrator extends BaseOrchestra
                 if (StringUtils.isBlank(item.getWardName()))
                     results.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, String.format(errorMessageResource.getString("NN_ERR01"), "wardName"), null));
 
-                if (StringUtils.isBlank(item.getIcd10Code()))
-                    results.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, String.format(errorMessageResource.getString("NN_ERR01"), "icd10Code"), null));
+//                if (StringUtils.isBlank(item.getIcd10Code()))
+//                    results.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, String.format(errorMessageResource.getString("NN_ERR01"), "icd10Code"), null));
 
                 if (StringUtils.isBlank(item.getWardId()))
                     results.add(new ResultDetail(ResultDetail.ResultsDetailsType.ERROR, String.format(errorMessageResource.getString("NN_ERR01"), "wardId"), null));
